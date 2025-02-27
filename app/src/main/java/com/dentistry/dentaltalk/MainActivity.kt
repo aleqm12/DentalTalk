5da7b8e9133a17c7b9d40b79eee6f02c22ef48ad
aleqm12 <qmolina.alejandro@gmail.com>
2025-02-27 16:59:47 -0600

    Metodo para cerrar sesion.

--- app/src/main/java/com/dentistry/dentaltalk/MainActivity.kt
+++ app/src/main/java/com/dentistry/dentaltalk/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.dentistry.dentaltalk
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,4 +25,22 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu_principal, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId){
+            R.id.menu_salir->{
+                FirebaseAuth.getInstance().signOut()
+                val intent = Intent(this@MainActivity, Inicio::class.java)
+                Toast.makeText(applicationContext,"Has cerrado sesión", Toast.LENGTH_SHORT).show()
+                startActivity(intent)
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 }
