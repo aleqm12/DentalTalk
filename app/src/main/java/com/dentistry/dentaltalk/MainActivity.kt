61f766b8ee22f644a5a737e2c42e411df60f136b
aleqm12 <qmolina.alejandro@gmail.com>
2025-03-04 10:43:42 -0600

    Categorias de usuarios y chats. Fragmentos.

--- app/src/main/java/com/dentistry/dentaltalk/MainActivity.kt
+++ app/src/main/java/com/dentistry/dentaltalk/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.dentistry.dentaltalk
 
+import android.app.FragmentManager
 import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
@@ -10,6 +11,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentPagerAdapter
+import androidx.viewpager.widget.ViewPager
+import com.dentistry.dentaltalk.Fragmentos.FragmentoChats
+import com.dentistry.dentaltalk.Fragmentos.FragmentoUsuarios
+import com.google.android.material.tabs.TabLayout
 import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
@@ -17,11 +24,50 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        InicializarComponentes()
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+    }
+
+    fun InicializarComponentes(){
+        val tabLayout: TabLayout = findViewById(R.id.TabLayoutMain)
+        val viewPager : ViewPager = findViewById(R.id.ViewPagerMain)
+
+        val viewPagerAdapter = ViewPagerAdapter(supportFragmentManager)
+
+        viewPagerAdapter.addItem(FragmentoUsuarios(), "Usuarios")
+        viewPagerAdapter.addItem(FragmentoChats(), "Chats")
+
+        viewPager.adapter = viewPagerAdapter
+        tabLayout.setupWithViewPager(viewPager)
+
+    }
+
+    class ViewPagerAdapter(fragmentManager: androidx.fragment.app.FragmentManager):FragmentPagerAdapter(fragmentManager) {
+
+
+        private val listaFragmentos: MutableList<Fragment> = ArrayList()
+        private val listaTitulos: MutableList<String> = ArrayList()
+
+        override fun getCount(): Int {
+           return listaFragmentos.size
+        }
+
+        override fun getItem(position: Int): Fragment {
+            return listaFragmentos[position]
+        }
+
+        override fun getPageTitle(position: Int): CharSequence? {
+            return listaTitulos[position]
+        }
+
+        fun addItem(fragment: Fragment, titulo:String){
+            listaFragmentos.add(fragment)
+            listaTitulos.add(titulo)
         }
     }
 
