27d7060f7ab30e84330d7fd9e5dc6a12f0934b1f
aleqm12 <qmolina.alejandro@gmail.com>
2025-03-11 12:11:51 -0600

    Buscar usuarios registrados en la base de datos

--- app/src/main/java/com/dentistry/dentaltalk/Fragmentos/FragmentoUsuarios.kt
+++ app/src/main/java/com/dentistry/dentaltalk/Fragmentos/FragmentoUsuarios.kt
@@ -1,10 +1,13 @@
 package com.dentistry.dentaltalk.Fragmentos
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.dentistry.dentaltalk.Adaptador.AdaptadorUsuario
@@ -23,6 +26,7 @@ class FragmentoUsuarios : Fragment() {
     private var usuarioAdaptador : AdaptadorUsuario?=null
     private var usuarioLista : List <Usuario>?=null
     private var rvUsuarios: RecyclerView?=null
+    private lateinit var Et_buscar_usuario : EditText
 
 
     override fun onCreateView(
@@ -35,10 +39,29 @@ class FragmentoUsuarios : Fragment() {
         rvUsuarios = view.findViewById(R.id.RV_usuarios)
         rvUsuarios!!.setHasFixedSize(true)
         rvUsuarios!!.layoutManager = LinearLayoutManager (context)
+        Et_buscar_usuario = view.findViewById(R.id.Et_buscar_usuario)
+
 
 
         usuarioLista = ArrayList()
         ObtenerUsuarioBD()
+
+        Et_buscar_usuario.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(b_usuario: CharSequence?, start: Int, before: Int, count: Int) {
+
+                BuscarUsuario(b_usuario.toString().lowercase())
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+
+        })
 
 
 
@@ -49,6 +72,35 @@ class FragmentoUsuarios : Fragment() {
         val firebaseUser = FirebaseAuth.getInstance().currentUser!!.uid
         val reference = FirebaseDatabase.getInstance().reference.child("Usuarios").orderByChild("n_usuario")
         reference.addValueEventListener(object: ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                (usuarioLista as ArrayList<Usuario>).clear()
+
+                if (Et_buscar_usuario.text.toString().isEmpty()){
+
+                    for (sh in snapshot.children){
+                        val usuario : Usuario?=sh.getValue(Usuario::class.java)
+                        if (!(usuario!!.getUid()).equals(firebaseUser)){
+                            (usuarioLista as ArrayList<Usuario>).add(usuario)
+                        }
+                    }
+
+                    usuarioAdaptador = AdaptadorUsuario(context!!, usuarioLista!!)
+                    rvUsuarios!!.adapter = usuarioAdaptador
+
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
+    }
+
+    private fun BuscarUsuario (buscarUsuario : String){
+        val firebaseUser = FirebaseAuth.getInstance().currentUser!!.uid
+        val consulta = FirebaseDatabase.getInstance().reference.child("Usuarios").orderByChild("buscar")
+            .startAt(buscarUsuario).endAt(buscarUsuario + "\uf8ff")
+        consulta.addValueEventListener(object : ValueEventListener{
             override fun onDataChange(snapshot: DataSnapshot) {
                 (usuarioLista as ArrayList<Usuario>).clear()
                 for (sh in snapshot.children){
@@ -65,7 +117,17 @@ class FragmentoUsuarios : Fragment() {
             override fun onCancelled(error: DatabaseError) {
                 TODO("Not yet implemented")
             }
-        })
-    }
 
+
+        })
+
+
+
+
+
+
+
+
+
+    }
 }
