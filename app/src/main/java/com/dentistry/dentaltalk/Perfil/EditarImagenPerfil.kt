699d6d7091c0bacc3f6bd34ea78e2c1e614b153f
aleqm12 <qmolina.alejandro@gmail.com>
2025-03-19 11:50:28 -0600

    Abrir y tomar una imagen de camara

--- app/src/main/java/com/dentistry/dentaltalk/Perfil/EditarImagenPerfil.kt
+++ app/src/main/java/com/dentistry/dentaltalk/Perfil/EditarImagenPerfil.kt
@@ -1,9 +1,11 @@
 package com.dentistry.dentaltalk.Perfil
 
 import android.app.Dialog
+import android.content.ContentValues
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
@@ -66,9 +68,30 @@ class EditarImagenPerfil : AppCompatActivity() {
                 Toast.makeText(applicationContext, "Cancelado por el usuario", Toast.LENGTH_SHORT).show()
             }
         }
-
-
     )
+
+    private fun AbrirCamara (){
+        val values = ContentValues()
+        values.put(MediaStore.Images.Media.TITLE, "Titulo")
+        values.put(MediaStore.Images.Media.DESCRIPTION, "Descripcion")
+        imageUri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,values)
+
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        intent.putExtra(MediaStore.EXTRA_OUTPUT,imageUri)
+        camaraActivityResultLauncher.launch(intent)
+    }
+
+    private  val camaraActivityResultLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()){resultado_camara->
+            if (resultado_camara.resultCode == RESULT_OK){
+                ImagenPerfilActualizar.setImageURI(imageUri)
+            }else{
+                Toast.makeText(applicationContext, "Cancelado por el usuario", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
+
     private fun MostrarDialog(){
         val Btn_abrir_galeria: Button
         val Btn_abrir_camara : Button
@@ -88,7 +111,8 @@ class EditarImagenPerfil : AppCompatActivity() {
         }
 
         Btn_abrir_camara.setOnClickListener {
-            Toast.makeText(applicationContext, "Abrir Camara", Toast.LENGTH_SHORT).show()
+            //Toast.makeText(applicationContext, "Abrir Camara", Toast.LENGTH_SHORT).show()
+            AbrirCamara()
             dialog.dismiss()
         }
 
